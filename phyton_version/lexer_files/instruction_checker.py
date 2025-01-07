402d16f89602974e75ac538509eacdb7da22478c
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-01-07 20:45:42 +0100

    [FIX] func / func_call / init / attri basic detection in syntax check

--- phyton_version/lexer_files/instruction_checker.py
+++ phyton_version/lexer_files/instruction_checker.py
@@ -50,29 +50,30 @@ def check_error_message(file, token):
         raise c15SyntaxError("Multiple error message defined one a single ligne", token.x_start, token.x_end, token.y, file[token.y])
 
 def instruction_checker(file, insts, inst, i):
-    """print()
+    print()
     for tok in inst:
-        print(tok)"""
+        print(tok)
     f_tok = inst[0]
     size = len(inst)
 
-    if f_tok.type == "key_word" and f_tok.id == "k_get":
+    if size >= 4 and f_tok.type == "key_word" and f_tok.id == "k_get":
         return
-    elif size >= 2 and f_tok.type == "data_type" and inst[-1].type == "identifier":
-        return check_initialization(file, inst)
-    elif size >= 4 and (f_tok.type == "data_type" or (f_tok.type == "key_word" and f_tok.id == "k_none")):
-        if any((tok.type == "operator" and tok.id == "o_attribution") for tok in inst):
-            return check_initialization(file, inst)
-        if any((tok.type == "identifier") for tok in inst):
-            return check_function(file, inst)
-    elif size >= 3 and f_tok.type == "identifier" and (inst[1].type == "operator" and (inst[1].id == "o_attribution" or inst[1].id == "o_operator_atribution")):
-        return check_attribution(file, inst)
-    elif size >= 3 and f_tok.type == "identifier" and inst[1].type == "delimitor" and inst[1].id == "d_round_1":
-        return check_function_call(file, inst)
-    elif any((tok.type == "key_word" and tok.id == "k_read") for tok in inst):
+    elif size == 1 and f_tok.type == "delimitor" and f_tok.id == "d_curly_2":
+        return
+    elif size == 7 and any((tok.type == "key_word" and tok.id == "k_read") for tok in inst):
         return check_read(file, inst)
-    elif any((tok.type == "key_word" and tok.id == "k_print") for tok in inst):
+    elif size == 7 and any((tok.type == "key_word" and tok.id == "k_print") for tok in inst):
         return check_print(file, inst)
+    elif size >= 2 and any((tok.type == "identifier") for tok in inst):
+        if size >= 2 and any((tok.type == "data_type") for tok in inst) or any((tok.type == "key_word" and tok.id == "k_none") for tok in inst):
+            if size >= 4 and any((tok.type == "delimitor" and tok.id == "d_round_1") for tok in inst):
+                return check_function(file, inst)
+            elif size >= 2 and not any((tok.type == "key_word" and tok.id == "k_none") for tok in inst):
+                return check_initialization(file, inst)
+        elif size >= 3 and any((tok.type == "delimitor" and tok.id == "d_round_1") for tok in inst):
+            return check_function_call(file, inst)
+        elif size >= 3 and any((tok.type == "operator" and tok.id == "o_attribution") for tok in inst) or any((tok.type == "operator" and tok.id == "o_operator_atribution") for tok in inst):
+            return check_attribution(file, inst)
     elif size == 1 and f_tok.type == "key_word" and f_tok.id == "k_error":
         return check_error_message(file, f_tok)
     x_end = max(tok.x_end for tok in inst if tok.y == f_tok.y)
