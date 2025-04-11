f5a00b19ae23c827a9f9c8cf8b6a1378e2ed3dc4
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-11 22:51:04 +0200

    feat: Clean usless file in the branch

--- phyton_version/lexer_files/checker/read_checker.py
+++ phyton_version/lexer_files/checker/read_checker.py
@@ -5,7 +5,7 @@
 ## Checker for read
 ##
 
-from lexer import check_operation
+from lexer import is_operation_tok, check_operation
 from error import c15SyntaxError
 
 def check_read(file, inst):
@@ -20,20 +20,31 @@ def check_read(file, inst):
     if x_end == inst[-1].x_end:
         x_end += 1
     
-    if size != 7:
-        arg = ", too mutch things"
-        if size < 7:
-            arg = ", less things than needed"
-        raise c15SyntaxError("Invalid delimitation of read" + arg + read_declaration, 1, x_end, read_tok.y, file[read_tok.y])
+    if size < 7:
+        raise c15SyntaxError("Invalid delimitation of read, less things than needed" + read_declaration, 1, x_end, read_tok.y, file[read_tok.y])
     if sum(1 for tok in inst if (tok.type == "key_word" and tok.id == "k_read")) != 1:
         raise c15SyntaxError("Multiple \'>>>\', only one at the same time" + read_declaration, 1, x_end, read_tok.y, file[read_tok.y])
-    if inst[5] != read_tok:
+    if inst[-2] != read_tok:
         raise c15SyntaxError("Invalid positionement of \'>>>\'" + read_declaration, read_tok.x_start, read_tok.x_end , read_tok.y, file[read_tok.y])
-    if not (inst[0].type == "delimitor" and inst[0].id == "d_round_1") or not (inst[2].type == "delimitor" and inst[2].id == "d_comma") or not (inst[4].type == "delimitor" and inst[4].id == "d_round_2"):
+    if not (inst[0].type == "delimitor" and inst[0].id == "d_round_1") or sum(1 for tok in inst if (tok.type == "delimitor" and tok.id == "d_comma")) != 1 or not (inst[-3].type == "delimitor" and inst[-3].id == "d_round_2"):
         raise c15SyntaxError("Invalid delimitation of read" + read_declaration, 1, read_tok.x_start - 1, read_tok.y, file[read_tok.y])
+    i = 1
+    while not (inst[i].type == "delimitor" and inst[i].id == "d_comma") and inst[i] != inst[-3] and is_operation_tok(inst[i]):
+        i += 1
+    if inst[i] == inst[-3]:
+        raise c15SyntaxError("Need a \',\' between \'(\' and \')\'" + read_declaration, inst[1].x_start, x_end, read_tok.y, file[read_tok.y])
+    elif not (inst[i].type == "delimitor" and inst[i].id == "d_comma"):
+        raise c15SyntaxError("Invalid thing in the operation for the stream value" + print_declaration, inst[i].x_start, inst[i].x_end, inst[i].y, file[inst[i].y])
+    check_operation(file, inst[1:i])
+    for tok in inst[i + 1:-3]:
+        if not is_operation_tok(tok):
+            raise c15SyntaxError("Invalid thing in the operation for the nbr_to_read value" + print_declaration, tok.x_start, tok.x_end, tok.y, file[tok.y])
+    check_operation(file, inst[i + 1:-3])
+    """
     if not (inst[1].type == "literal" and (inst[1].id == "l_integer" or (inst[1].id == "l_binary" and inst[1].value[0] != "0"))) and not inst[1].type == "identifier":
         raise c15SyntaxError("Invalid stream value, stream need to be an integer > 0" + read_declaration, inst[1].x_start, inst[1].x_end, read_tok.y, file[read_tok.y])
     if not (inst[3].type == "literal" and (inst[3].id == "l_integer" or (inst[3].id == "l_binary" and inst[3].value[0] != "0"))) and not inst[3].type == "identifier":
         raise c15SyntaxError("Invalid nbr_to_read value, nbr_to_read need to be an integer >= 0" + read_declaration, inst[3].x_start, inst[3].x_end, read_tok.y, file[read_tok.y])
-    if not (inst[6].type == "literal" and inst[6].id == "l_string") and not inst[6].type == "identifier":
-        raise c15SyntaxError("Invalid variable to write in, buffer need to be a str variable" + read_declaration, inst[6].x_start, inst[6].x_end, read_tok.y, file[read_tok.y])
+    """
+    if not (inst[-1].type == "literal" and inst[-1].id == "l_string") and not inst[-1].type == "identifier":
+        raise c15SyntaxError("Invalid variable to write in, buffer need to be a str variable" + read_declaration, inst[-1].x_start, inst[-1].x_end, read_tok.y, file[read_tok.y])
