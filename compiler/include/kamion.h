504dc60691c74f1c71c6815464c96b5d8238ef94
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-16 00:20:57 +0200

    feat: usefull thing, is_valid_dir, is_valid_file

--- compiler/include/kamion.h
+++ compiler/include/kamion.h
@@ -1,9 +1,21 @@
-/*
-** EPITECH PROJECT, 2025
-** kamion.h
-** File description:
-** Kamion header
-*/
+/**************************************************************\
+
+ ██╗  ██╗ █████╗ ██████╗ ████████╗ █████╗ ███╗   ██╗██╗ █████╗ 
+ ╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗████╗  ██║██║██╔══██╗
+  ╚███╔╝ ███████║██████╔╝   ██║   ███████║██╔██╗ ██║██║███████║
+  ██╔██╗ ██╔══██║██╔══██╗   ██║   ██╔══██║██║╚██╗██║██║██╔══██║
+ ██╔╝ ██╗██║  ██║██║  ██║   ██║   ██║  ██║██║ ╚████║██║██║  ██║
+ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
+
+Edition:
+##  15/04/2025 by Tsukini
+
+File Name:
+##  kamion.h
+
+File Description:
+## The main header of the kamion compiler
+\**************************************************************/
 
 #ifndef KAMION_H
     #define KAMION_H
@@ -25,6 +37,8 @@ typedef struct compiler_s {
     char const *exe_name;
 
     /* option */
+    bool errors;
+    char *binary;
 
     /* token id */
     hashtable_t *id;
@@ -47,11 +61,20 @@ int kamion(int const argc, char const *argv[], compiler_t *data); // Error: KO
 /* init_data */
 int init_data(compiler_t *data); // Error: KO
 int init_global(compiler_t *data); // Error: KO
+int init_option(compiler_t *data); // Error: KO
 
 /* init_flag */
 int init_flag(compiler_t *data, int const argc, char const *argv[]); // Error: KO
 int flag_help(void); // Error: KO
 int flag_null(compiler_t *data, int const argc, char const *argv[]); // Error: KO
+int flag_binary(compiler_t *data, int const argc, char const *argv[]); // Error: KO
+int flag_Errors(compiler_t *data, int const argc, char const *argv[]); // Error: KO
+int flag_directory(compiler_t *data, int const argc, char const *argv[]); // Error: KO
+int flag_Directory(compiler_t *data, int const argc, char const *argv[]); // Error: KO
+
+/* useful */
+bool is_valid_dir(compiler_t *data, char const *path); // Error: false
+bool is_valid_file(compiler_t *data, char const *path); // Error: false
 
 /* exit */
 int free_data(compiler_t *data); // Error: KO
